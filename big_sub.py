8717b3c77ee149ffb355367aaa2201dc96f54d68
Ayush Tiwari <teltorob@gmail.com>
2022-03-21 18:34:10 +0530

    this was not working and also not plagarised

--- big_sub.py
+++ big_sub.py
@@ -1,1 +1,23 @@
-def big_sub(a, b):
+fun sub(a, b) =
+        let
+                fun subhelper([], [], carry) =
+                        if (carry=0) then [] else [] 
+                  | subhelper(xs,[],carry) =
+                  if carry = 0 
+                  then xs
+                  else subhelper(xs,[carry],0)
+
+                  | subhelper([],ys,carry) = subhelper(ys,[carry],0)
+                  | subhelper(x::xs,y::ys,carry) =
+                          let
+                                val z = x-y-carry
+                          in
+                          if (z >= 0)
+                          then
+                                (z)::subhelper(xs,ys,0)
+                            else
+                            (z + 10)::subhelper(xs,ys,1)
+                          end
+        in
+                subhelper(a,b,0)
+        end;
